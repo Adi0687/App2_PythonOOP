0cdef36bb9cd19ec3de48e975705065842f0a497
Aadil Feroze <ferozeaadil@gmail.com>
2021-10-08 13:25:17 +0300

    Classes made and pdf generated!

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
     Calculates the total amount that needs to be paid
@@ -18,8 +20,9 @@ class Flatmate:
         self.name = name
         self.days_in_flat = days_in_flat
 
-    def pays(self, bill):
-        return bill.amount / 2
+    def pays(self, bill, flatmate2):
+        to_pay = bill.amount * (self.days_in_flat/(self.days_in_flat + flatmate2.days_in_flat))
+        return to_pay
 
 
 class Pdf:
@@ -31,7 +34,36 @@ class Pdf:
         self.filename = filename
 
     def generate_pdf(self, flatmate1, flatmate2, bill):
-        pass
+
+        flatmate1_payment = str(round(flatmate1.pays(bill, flatmate2),2))
+        flatmate2_payment = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add Icon
+        pdf.image("files/house.png", w=30, h=30)
+
+        # Title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align='C', ln=1)
+
+        # Heading
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=25, txt="Period", border=0)
+        pdf.cell(w=150, h=25, txt=bill.period, border=0, ln=1)
+
+        # # Flatmate1 Detail
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=100, h=25, txt= flatmate1_payment, border=0, ln=1)
+
+        # # Flatmate2 Detail
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=1)
+        pdf.cell(w=100, h=40, txt= flatmate2_payment, border=1, ln=1)
+
+        pdf.output(self.filename)
+
 
 
 the_bill = Bill(amount=120, period="Dec 2021")
@@ -39,4 +71,9 @@ aadil = Flatmate(name="Aadil", days_in_flat=25)
 shadia = Flatmate(name="Shadia", days_in_flat=30)
 
 print(the_bill.amount)
-print(aadil.pays(bill=the_bill))
+print("Aadil Pays: ", aadil.pays(bill=the_bill, flatmate2=shadia))
+print("Shadia Pays: ", shadia.pays(bill=the_bill, flatmate2=aadil))
+
+filename = the_bill.period + " Report.pdf"
+pdf_report = Pdf(filename=filename)
+pdf_report.generate_pdf(flatmate1=aadil, flatmate2=shadia, bill=the_bill)
